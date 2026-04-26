9c3989fc7a0df14251597a257a968a7aad42d41e
Ananya Mishra <code.ananyamishra@gmail.com>
2026-04-26 12:37:39 +0530

    ADD

---
+++ src/Day1/College.java
@@ -0,0 +1,11 @@
+package Day1;
+import java.util.Scanner;
+
+public class College {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your college name: ");
+        String college = sc.nextLine();
+        System.out.print("My college name is "+ college);
+    }
+}
